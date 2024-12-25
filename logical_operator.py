fec6dfca6c7efca3dc68051c4485e3c969377da4
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2024-12-26 02:31:20 +0600

    truth table of 'or' 'and' operator.

---
+++ logical_operator.py
@@ -0,0 +1,10 @@
+# Truth table of 'or'
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+# Truth table of 'and'
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
