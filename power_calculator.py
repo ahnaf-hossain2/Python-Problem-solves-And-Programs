08869af77cb105d0eb19b95e08fd504f177578fa
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2024-12-27 16:57:06 +0600

    Implement power calculation for user input number

---
+++ power_calculator.py
@@ -0,0 +1,11 @@
+# Create a program that takes a number as input from the user and
+# calculates its cube (the number raised to the power of 3).
+# The program should display the result with a clear message 
+# indicating the calculated cube value
+#----------------------------------------------------------------
+
+# taking input of the integer number.
+a = int(input("Enter the number :"))
+
+# Use ** to find the power of any number.
+print("The cube of the number is : ", a**3 )
