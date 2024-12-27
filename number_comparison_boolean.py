dece44fa3b9b6b9274d19853e67f61f5638e300d
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2024-12-27 16:04:56 +0600

    Implement boolean comparison between two user input numbers

---
+++ number_comparison_boolean.py
@@ -0,0 +1,12 @@
+# Create a program that takes two integer inputs from the user and 
+# outputs a boolean value indicating whether the first number is 
+# greater than the second number. The program should clearly display 
+# the comparison result using a descriptive message.
+#----------------------------------------------------------------------
+
+# taking input of integer numbers for a and b.
+a = int(input("Enter number 1:"))
+b = int(input("Enter number 2:"))
+
+# It will shpw if a is greater than b is ture or false.
+print("a is greater than b is ", a>b)
