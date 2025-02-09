d3e00d57e272667e7bb0cdf8280a753d529904f7
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-09 22:49:29 +0600

    fix: include the upper limit in the series sum calculation in Series1.py

--- Problem Solves/Series1.py
+++ Problem Solves/Series1.py
@@ -1,7 +1,7 @@
 # 1 + 2 + 3 + ..... + n series
 n = int(input("Enter the range(n): "))
 sum = 0
-for i in range (1,n):
+for i in range (1,n+1):
     print(i,end=" ")
-    sum = sum + i 
+    sum = sum + i
 print("\nSum = ",sum)
