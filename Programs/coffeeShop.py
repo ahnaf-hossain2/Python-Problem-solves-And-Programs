16a42eab0360e204339cbc5a6481335ba4187cd3
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-11 11:40:12 +0600

    remove: delete coffee shop interaction code from coffeeShop.py

--- Programs/coffeeShop.py
+++
@@ -1,9 +0,0 @@
-print("Hello, welcome to Peak Coffee!!")
-
-name = input("What's your name ?\n-> ")
-print("Hey! "+ name + ", thank you so much for coming in today!")
-
-menu = "1.Black Coffee,\n2.Espresso,\n3.Latte,\n4.Cappucino"
-print(name + "What would you like from out menu today? Here is what we are serving.\n" + menu )
-print(input( "->"))
-print("Sounds good " + name + ", we'll have that ready for you in a moment.")
