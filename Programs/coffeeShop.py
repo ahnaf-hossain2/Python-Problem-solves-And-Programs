2b0e430b358491871b7dbb458570bb143898c41c
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-11 11:09:51 +0600

    fix: remove smiley face from coffee shop response message

--- Programs/coffeeShop.py
+++ Programs/coffeeShop.py
@@ -6,4 +6,4 @@ print("Hey! "+ name + ", thank you so much for coming in today!")
 menu = "1.Black Coffee,\n2.Espresso,\n3.Latte,\n4.Cappucino"
 print(name + "What would you like from out menu today? Here is what we are serving.\n" + menu )
 print(input( "->"))
-print("Sounds good " + name + ", we'll have that ready for you in a moment ^_^.")
+print("Sounds good " + name + ", we'll have that ready for you in a moment.")
