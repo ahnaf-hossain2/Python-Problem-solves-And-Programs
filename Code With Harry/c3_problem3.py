8cd3e2a6fb4d72f426f83f0a5ecd3a4c579b7546
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-13 15:25:05 +0600

    add: enhance string manipulation in c3_problem3.py to replace double spaces and demonstrate string immutability

--- Code With Harry/c3_problem3.py
+++ Code With Harry/c3_problem3.py
@@ -1,2 +1,7 @@
 name = "Hello my name is  Ahnaf"
 print (name.find("  "))
+
+print(name.replace("  ", " "))
+
+# But printing name directly will give the original name
+print(name) # strings are immutable which means that you cannot change them by running functions on them
