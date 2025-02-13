2331edaf201887a4b842ee4f7ced08cc1423e750
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-13 19:59:28 +0600

    add: implement basic student marks input and sorting in c4_problem1.py; add sum calculation in c4_problem2.py; add tuple count example in c4_problem3.py

---
+++ Code With Harry/c4_problem1.py
@@ -0,0 +1,10 @@
+# fruits = []
+# for i in range(7):
+#     fruits.append(input("Enter fruits name: "))
+# print("Fruits:",fruits)
+
+marks = []
+for j in range(6):
+    marks.append(int(input("Enter Marks of student :")))
+marks.sort()
+print(marks)
---
+++ Code With Harry/c4_problem2.py
@@ -0,0 +1,9 @@
+num = [1, 2, 3, 4, 5]
+print(sum(num))
+
+# nums = [34, 23, 645, 123]
+
+# sum = 0
+# for i in nums:
+#     sum = sum + i
+# print(sum)
