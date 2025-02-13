d7eadb07dea09dc7036c4724fe9c309d97a43622
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-14 01:22:40 +0600

    add: implement name checking program in c6_program5.py

---
+++ Code With Harry/c6_program5.py
@@ -0,0 +1,6 @@
+# name = input("Enter your name: ")
+# print("All is fine" if len(name)>=10 else "Contains less than 10 characters")
+
+n = ["Ahnaf","Pulok"]
+gn = input("Enter a name to check: ")
+print("Your name is in the list" if gn in n else "Not in the list")
