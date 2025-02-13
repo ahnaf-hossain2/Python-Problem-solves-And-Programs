02732fd651d717a7965a4016f43f51684c6a8f65
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-14 00:13:00 +0600

    add: implement program to find the greatest of four numbers in c6_program2.py

---
+++ Code With Harry/c6_program2.py
@@ -0,0 +1,13 @@
+n1 = int(input("Enter number 1: "))
+n2 = int(input("Enter number 2: "))
+n3 = int(input("Enter number 3: "))
+n4 = int(input("Enter number 4: "))
+
+if(n1>n2 and n1>n3 and n1>n4):
+    print("Number 1 is greatest")
+elif(n2>n1 and n2>n3 and n2>n4):
+    print("Number 2 is greatest")
+elif(n3>n1 and n3>n2 and n3>n4):
+    print("Number 3 is greatest")
+elif(n4>n1 and n4>n2 and n4>n3):
+    print("Number 4 is greatest")
