53d6244131e40675578466fc1b1bf8737c3230f3
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-14 11:15:44 +0600

    update: refactor multiplication table implementation to use a while loop in c7_problem1.py

--- Code With Harry/c7_problem1.py
+++ Code With Harry/c7_problem1.py
@@ -1,5 +1,10 @@
 num = int(input("Enter a number: "))
-mul = 1
-for i in range(1,11):
-    mul = num * i
-    print(f"{num} X {i} = {mul}")
+# mul = 1
+# for i in range(1,11):
+#     mul = num * i
+#     print(f"{num} X {i} = {mul}")
+
+i = 1
+while i <= 10:
+    print(f"{num} X {i} = { i * num}")
+    i += 1
