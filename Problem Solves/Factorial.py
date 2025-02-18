c15a38599cd08abe6ab7558d71640efffef507e4
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-18 10:16:20 +0600

    add: add recursive implementation for factorial calculation in Factorial.py

--- Problem Solves/Factorial.py
+++ Problem Solves/Factorial.py
@@ -4,3 +4,12 @@ fact = 1
 for i in range(1,n+1):
     fact*=i
 print("Factorial of n: ",fact)
+
+# using recursion
+# def factorial(n):
+#     if n == 0 or n == 1:  # Base case
+#         return 1
+#     else:
+#         return n * factorial(n - 1)  # Recursive case
+
+# print(factorial(5))  # Output: 120
