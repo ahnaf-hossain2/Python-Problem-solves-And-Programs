1eaac97445cda1985ff52fb6ed69809088d762c6
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-18 11:09:00 +0600

    fix: update pattern function to handle zero rows and improve input handling

--- Code With Harry/c8_program4.py
+++ Code With Harry/c8_program4.py
@@ -6,8 +6,11 @@ for n = 3
 '''
 
 def pattern(n):
-    if n == 1:
-        print('*')
+    if n == 0:
+        return
     else:
         print('*' * n)
         pattern( n-1 )
+
+n = int(input('Enter the number of rows: '))
+pattern(n)
