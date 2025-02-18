d9018788b55f65e52cf0482aa197cb15a50546b7
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-18 21:27:59 +0600

    add: migrate file reading functionality to c9_fileRead.py and remove c9_problem1.py

---
+++ Code With Harry/c9_fileRead.py
@@ -0,0 +1,4 @@
+file = open("Python-Problem-solves-And-Programs/code with harry/name.txt") # by default read mode
+data = file.read()
+print(data)
+file.close()
--- Code With Harry/c9_problem1.py
+++
@@ -1,4 +0,0 @@
-file = open("Python-Problem-solves-And-Programs/code with harry/name.txt")
-data = file.read()
-print(data)
-file.close()
