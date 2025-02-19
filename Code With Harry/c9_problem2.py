e7c565a62951abfa1916b7d97c8d3f6bf5cbadc5
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-19 08:13:31 +0600

    fix: correct variable naming for high score in c9_problem2.py

--- Code With Harry/c9_problem2.py
+++ Code With Harry/c9_problem2.py
@@ -6,19 +6,19 @@ def game():
     # Generate a random score between 1 and 10
     score = random.randint(1, 10)
 
-    # Fetch the previous highscore
+    # Fetch the previous highScore
     with open("Python-Problem-solves-And-Programs/Code With Harry/Hi-score.txt", "r") as f:
-        highscore = f.read()
-        if highscore == '':
-            highscore = 0  # Set highscore to 0 if file is empty
+        highScore = f.read()
+        if highScore == '':
+            highScore = 0  # Set highScore to 0 if file is empty
         else:
-            highscore = int(highscore)  # Convert highscore to integer
+            highScore = int(highScore)  # Convert highScore to integer
 
     print(f"The current score is {score}")
 
-    # Check if the current score is higher than the highscore
-    if score > highscore:
-        # Write the new highscore to the file
+    # Check if the current score is higher than the highScore
+    if score > highScore:
+        # Write the new highScore to the file
         with open("Python-Problem-solves-And-Programs/Code With Harry/Hi-score.txt", "w") as f:
             f.write(str(score))
         print("New high score!")
