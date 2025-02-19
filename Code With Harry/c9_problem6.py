ca1e560b85d0b6577428d6734fcb1d41ac3d0482
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-20 01:17:59 +0600

    add: implement presence check for 'Python' in replace.txt

---
+++ Code With Harry/c9_problem6.py
@@ -0,0 +1,9 @@
+with open("Python-Problem-solves-And-Programs/Code With Harry/replace.txt", "r") as file:
+    content = file.read()
+
+if ("Python" in content):
+    print("Python is present")
+else:
+    print("Python is not present")
+
+
