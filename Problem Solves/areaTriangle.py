cecb8e04c1af2ec49518c049ca019e516b9a7072
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-21 01:31:02 +0600

    add: refactor triangle area calculation into Triangle class with display method

--- Problem Solves/areaTriangle.py
+++ Problem Solves/areaTriangle.py
@@ -1,5 +1,24 @@
+'''
 base = int(input("Enter the base: "))
 height = int(input("Enter the height: "))
 
 area = 0.5 * base * height
 print("The area of Triangle is: ", area)
+'''
+
+class Triangle:
+    # base = "" not needed to declare here
+    # height = ""
+    
+    def __init__ (self, base, height):
+        self.base = base
+        self.height = height
+
+    def display(self):
+        area = 0.5 * self.base * self.height
+        print("The area of Triangle is: ", area)
+
+T1 = Triangle(10, 20)
+T2 = Triangle(20, 30)
+T1.display()
+T2.display()
