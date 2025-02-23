0e41ae7e359d663b56bbdb235376117b32c26a73
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-02-23 17:00:54 +0600

    refactor: move greet method to the top of the Calculator class

--- Code With Harry/c10_problem2.py
+++ Code With Harry/c10_problem2.py
@@ -2,15 +2,14 @@
 # number.
 
 class Calculator:
-
+    @staticmethod # This method can be called without creating an object of the class
+    def greet():
+        print("Hello")
     def display(self, num):
         print(num ** 2)
         print(num ** 3)
         print(num ** 0.5)
-    @staticmethod # This method can be called without creating an object of the class
-    def greet():
-        print("Hello")
-
+        
 number = Calculator()
 number.greet()
 number.display(5)
