ba6aadc1585d1477ea7d3ca2efd85ed9e030c74e
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-03-02 19:31:20 +0600

    update: modify fruit list comprehension to replace 'banana' with 'orange' and print results

--- Code With Harry/c10_problem4.py
+++ Code With Harry/c10_problem4.py
@@ -1,3 +1,3 @@
-ls = [1,2,3,4,5,6]
-newls = [f"{i} is an even number" for i in ls if i%2 == 0 ]
-print(newls)
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x if x != "banana" else "orange" for x in fruits]
+print(newlist)
