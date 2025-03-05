46069198956a3148d260f9d41e1b7f11ab0007c9
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-03-05 17:35:39 +0600

    add area calculation functions for triangle and rectangle

---
+++ Module/area.py
@@ -0,0 +1,5 @@
+def triangle_area(base, height):
+    print(f"Area of triangle is: {0.5 * base * height}")
+
+def rectangle_area(a, b):
+    print(f"Area of rectangle is: {a * b}")
---
+++ Module/test.py
@@ -0,0 +1,5 @@
+from area import triangle_area, rectangle_area # Importing functions from area.py
+
+# Calling functions from area.py
+triangle_area(10, 20)
+rectangle_area(10, 20)
