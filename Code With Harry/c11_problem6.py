c4edc77c63090b177c75e7ffb4b573f9964e9b39
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-03-07 23:35:25 +0600

    add multiplication method to Complex class

--- Code With Harry/c11_problem6.py
+++ Code With Harry/c11_problem6.py
@@ -7,9 +7,13 @@ class Complex:
     def __add__(self, c2):
         return Complex(self.r + c2.r, self.i + c2.i) # this returns the sum of two complex numbers
 
+    def __mul__(self, c2):
+        return Complex(self.r*c2.r - self.i*c2.i, self.r*c2.i + self.i*c2.r)
+
     def __str__(self):
         return f"{self.r} + {self.i}i"
 
 c1 = Complex(1, 2) # this creates a complex number 1 + 2i
 c2 = Complex(3, 4)
 print(c1 + c2) # this prints the sum of c1 and c2
+print(c1 * c2) # this prints the product of c1 and c2
