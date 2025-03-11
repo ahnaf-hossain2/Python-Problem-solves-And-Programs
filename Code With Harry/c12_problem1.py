2466cb665596556babc0c3d857802decf27ef831
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-03-11 07:34:25 +0600

    refactor c12_problem1.py to replace enumeration with list comprehension for squared even numbers

--- Code With Harry/c12_problem1.py
+++ Code With Harry/c12_problem1.py
@@ -1,9 +1,4 @@
-l = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
-# index = 0
-# for i in l:
-#     print(f"The item number at index {index} is {i}")
-#     index += 1
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# This can be simplified using enumerate function
-for index, item in enumerate(l):
-    print(f"The item number at index {index} is {item}")
+squaredlist = [i**2 for i in l if i % 2 == 0]
+print("Squared even numbers in the list are: ", squaredlist)
