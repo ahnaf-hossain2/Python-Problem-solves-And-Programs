4ed8fd484bf8be6a9582b08705243bda149326c4
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2026-08-09 00:33:19 +0600

    Just basic python practice

---
+++ Practice Bsics/basic.py
@@ -0,0 +1,11 @@
+# print("Hello") #-> This is for printing
+# Number = 5 #-> declare variable
+# print(type(Number)) #-> To know the type of the variable
+# if (5>2): #-> If condition
+#     print("Bigger") #-> Indentation Must follow
+# print("Hello"); print("World"); print("Bye") #To write multiple statements in one line
+"""
+# print("Hello world", end=" ") #end to print in the same line
+# print("This will print in the same line")
+""" # this is for multiline comment.
+# print("I am", 22, "years old.") #combine int and string
