5c4605751678f196d99c832d577d1a5de56d5386
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2026-09-17 23:42:12 +0600

    Add dictionary access example and set creation instructions

---
+++ Practice Bsics/02_dictionary_set.py
@@ -0,0 +1,22 @@
+"""
+Dictionary — Access Values
+Print: name, age, department
+Add Data - ABC University
+"""
+student = {
+    "name" : "Ahnaf",
+    "age" : 22,
+    "department" : "CSE"
+}
+
+student["name"] = "MD. Ahnaf Hossain"
+student["university"] = "ABC University"
+
+print(student["name"], student["age"], student[ "department"], student["university"])
+print()
+
+"""
+Set — Create
+Create a set containing: 1, 2, 3, 4, 5
+Print the set.
+"""
