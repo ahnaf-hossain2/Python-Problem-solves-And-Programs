6fb181ac3caa8f2c80fc13008bb0e4dc85af116f
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2026-09-19 00:17:39 +0600

    Add examples for printing dictionary keys and values, and demonstrate set operations

--- Practice Bsics/02_dictionary_set.py
+++ Practice Bsics/02_dictionary_set.py
@@ -23,3 +23,29 @@ Print the set.
 
 numbers = {1,2,2,3,5,4,5,1} # only 1,2,3,4,5 will be printed
 print(numbers)
+
+
+"""
+Now print all the keys also the value sof student using loop
+"""
+
+for i in student:
+    print(i)
+
+for i in student: # here i stores the keys
+    print(student[i])
+
+"""
+Set operations:
+Find:
+- Union
+- Intersection
+- Difference (a - b)
+"""
+
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+
+print(a|b)
+print(a&b)
+print(a-b)
