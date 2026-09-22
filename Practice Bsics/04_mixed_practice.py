83c2f0f8f0985370d7ea421da378e584a9be19e1
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2026-09-22 14:35:02 +0600

    some mixed dic + function problem solved

--- Practice Bsics/04_mixed_practice.py
+++ Practice Bsics/04_mixed_practice.py
@@ -36,4 +36,40 @@ def unique_set(num_list):
 
 n_list = [1,223,4,5,2,2,5,5,3,3,6,7,20]
 print(unique_set(n_list))
+print()
 
+"""
+Create a function: find_number(numbers, target)
+If target exists in the list, return the target.
+If it doesn't exist, return None.
+"""
+
+def find_number(numbers, target):
+    if target in numbers:
+        return target
+    else:
+        return None
+
+num = [22,55,1,4,6,8,100]
+print(find_number(num, 1))
+print(find_number(num, 11))
+print()
+
+"""
+Create a dictionary:
+Create a function that receives the dictionary and returns the average marks.
+"""
+
+def avg(student):
+    sum = 0
+    for i in student["marks"]:
+        sum = sum + i
+    avgmarks = sum/ len(student["marks"])
+    return avgmarks
+
+student = {
+    "name" : "Ahnaf",
+    "marks" : [80, 86, 90, 78]
+}
+
+print(avg(student))
